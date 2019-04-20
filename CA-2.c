7bcc1dc7e1472e2ffb85aa5e2476cbe9ce9d9712
Aditya Kumar Singh <38782886+adi19012001@users.noreply.github.com>
2019-04-20 21:41:12 +0530

    Add files via upload

--- CA-2.c
+++ CA-2.c
@@ -13,7 +13,9 @@ int main()
    Sleep(500);
    printf("Singh");
 
-   int p[50],br[50],ar[50],i,n,w[50],pr[50],tat[50],pr[50];
+   int ar[20],bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+   Sleep(500);
+   printf("\n\t\t\t\t\t-----Priority using Formula-----");
 
    /* Taking inputs from the user */
 
@@ -22,11 +24,82 @@ int main()
    printf("\nEnter the arrival time and burst time: ");
    for (i=0;i<n;i++)
     {
-        printf("\nP[%d]:" , i+1);
+        printf("\nP[%d]: \nArrival time: " , i+1);
         scanf("%d" , &ar[i]);
-        scanf("%d", &br[i]);
+        printf("\nBurst time: ");
+        scanf("%d", &bt[i]);
         p[i]=i+1;
    }
+
+   /* finding the waiting time and putting formula into in to find
+   the priority*/
+
+   for(i=0;i<n;i++)
+    {
+        wt[i]=0;    // waiting time for the first process is 0
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+
+        total+=wt[i];
+        pr[i]=1+wt[i]/bt[i];   // formula to calculate the priority of the process i
+        p[i]=i+1;
+         // contains the process number
+   }
+
+   /* sorting the process along with its properties using the
+   selection sort*/
+
+   for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+
+    // avg waiting time
+
+    avg_wt=total/n;
+    total=0;
+
+    // displaying the result
+    printf("\nLoading Result.");
+    Sleep(500);
+    printf(".");
+    Sleep(500);
+    printf(".");
+    Sleep(500);
+    printf(".");
+    printf("\n\t\t\t\t\t\t----RESULT----\n");
+
+
+    printf("\nProcess\t  Arrival Time\t    Burst Time    \tWaiting Time\tTurnaround Time\t\tPriority");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];     //calculate turnaround time
+        total+=tat[i];
+        printf("\nP[%d]\t\t  %d\t\t  %d\t\t    %d\t\t\t%d  \t\t%d",p[i],ar[i],bt[i],wt[i],tat[i],pr[i]);
+    }
+
+    avg_tat=total/n;     //average turn around time
+    printf("\n\nAverage Waiting Time=%d\n",avg_wt);
+    printf("\nAverage Turnaround Time=%d\n",avg_tat);
+
+
 
 
    //return main();
